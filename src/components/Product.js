7851f1797d03c7e6d72a468ebc1ccd694de5e864
DumitruVlad29 <dumitruvladut@yahoo.com>
2023-03-31 17:56:37 +0300

    redux + cart

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import { MdOutlineStar } from 'react-icons/md';
+import { useDispatch } from 'react-redux';
 import { useLocation } from "react-router-dom";
+import { addToCart } from '../redux/bazarSlice';
 
 const Product = () => {
+  const dispatch = useDispatch()
   const [details, setDetails] = useState({});
+  let [baseQty, setBaseQty] = useState(1);
   const location = useLocation()
   useEffect(() =>{
     setDetails(location.state.item);
@@ -30,10 +34,10 @@ const Product = () => {
           </div>
         <div className='w-3/5 flex flex-col justify-center gap-12'>
             <div>
-              <h2 className='text-4x1 font-semibold'>{details.title}</h2>
+              <h2 className='text-4xl font-bold'>{details.title}</h2>
               <div className='flex items-center gap-4 mt-3'>
                 <p className='line-through font-base text-gray-500'>${details.oldPrice}</p>
-                <p className='text-2x1 font-medium text-gray-900'>${details.price}</p>
+                <p className='text-2xl font-medium text-gray-900'>${details.price}</p>
               </div>
             </div>
             <div className='flex items-center gap-2'>
@@ -47,6 +51,48 @@ const Product = () => {
               <p className='text-xs text-gray-500'>(1 Customer review)</p>
              </div>
              <p className='text-base text-gray-500 -mt-3'>{details.description}</p>
+             <div className='flex gap-4'>
+              <div className='w-52 flex items-center justify-between text-gray-500 gap-4 border p-3'>
+                <p className='text-sm'>Quantity</p>
+              <div className='flex items-center gap-4 text-sm font-semibold'>
+              <button 
+              onClick={() => setBaseQty(baseQty ===1 ? (baseQty=1) : baseQty - 1)}
+              className='border h-5 font-normal text-lg flex items-center justify-center px-2 hover:bg-gray-700 hover:text-white cursor-pointer duration-300 active:bg-black'>
+                -
+                </button>
+                <span>
+                  {baseQty}
+                  </span>
+                <button 
+                onClick={() => setBaseQty(baseQty + 1)}
+                className='border h-5 font-normal text-lg flex items-center justify-center px-2 hover:bg-gray-700 hover:text-white cursor-pointer duration-300 active:bg-black'>
+                  +
+                  </button>
+              </div>
+              </div>
+              <button 
+              onClick={() => 
+                dispatch(
+                  addToCart({
+                    id: details._id,
+                title: details.title,
+                image: details.image,
+                price: details.price,
+                quantity: baseQty,
+                description: details.description,
+                  })
+                 )
+                }
+              className='bg-black text-white py-3 px-6 active:bg-gray-800'>
+                Add to cart
+                </button>
+             </div>
+             <p>
+              Category: 
+              <span className='font-medium capitalize'>
+                {details.category}
+                </span>
+                </p>
         </div>
       </div>
     </div>
--- src/components/ProductsCard.js
+++ src/components/ProductsCard.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
 import { BsArrowRight } from "react-icons/bs";
+import { useDispatch } from 'react-redux';
+import { addToCart } from "../redux/bazarSlice";
 
 const ProductsCard = ({ product }) => {
+  const dispatch = useDispatch()
   const navigate = useNavigate();
   const _id = product.title;
   const idString = (_id) => {
@@ -24,7 +27,7 @@ const ProductsCard = ({ product }) => {
         onClick={handleDetails}
         className='w-full h-96 cursor-pointer overflow-hidden'>
         <img 
-        className='w-full h-full object-cover group-hover:scale-110 duration-500'
+        className='w-full h-full object-fill group-hover:scale-110 duration-500'
         src={product.image} 
         alt="productImg" />
       </div>
@@ -40,7 +43,19 @@ const ProductsCard = ({ product }) => {
               <p className='font-semibold'>${product.price}</p>
               <p className='line-through text-gray-500'>${product.oldPrice}</p>
             </div>
-            <p className='absolute z-20 w-[100px] text-gray-500 hover:text-gray-900 flex items-center gap-1 top-0 transform -translate-x-32 group-hover:translate-x-0 transition-transform cursor-pointer duration-500'>add to cart<span><BsArrowRight /></span></p>
+            <p onClick={()=>
+            dispatch(
+              addToCart({
+                _id: product._id,
+                title: product.title,
+                image: product.image,
+                price: product.price,
+                quantity: 1,
+                description: product.description,
+              })
+              )
+            } 
+              className='absolute z-20 w-[100px] text-gray-500 hover:text-gray-900 flex items-center gap-1 top-0 transform -translate-x-32 group-hover:translate-x-0 transition-transform cursor-pointer duration-500'>add to cart<span><BsArrowRight /></span></p>
           </div>
         </div>
         <div>
